8f6110bb38d6bff2813591c7fbc7f30bcac3bf6b
amitab <amitab.marwaha@zohomail.in>
2026-07-02 15:54:18 +0530

    Added logging for project

--- src/main/java/com/utility/Base.java
+++ src/main/java/com/utility/Base.java
@@ -1,5 +1,8 @@
 package com.utility;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,11 +11,13 @@ import java.util.Properties;
 
 public class Base {
 
-    //load properties file
+    //initialize objects for properties file and logger
     protected static Properties properties = new Properties();
     protected static String propertyFilePath = "src/main/resources/properties/config.properties";
+    protected static Logger logger = LogManager.getLogger();
 
     static {
+        logger.info("loading properties file...");
         try (BufferedReader br = new BufferedReader(new FileReader(propertyFilePath))) {
             properties.load(br);
         } catch (IOException e) {
